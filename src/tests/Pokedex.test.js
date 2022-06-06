0f0114a77ff2c96b71329be7d7bd056e40e4512c
Aron Adams <aronrapetto@hotmail.com>
2022-06-06 19:53:58 -0300

    requisito 5 em desenvolvimento

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Pokedex from '../pages/Pokedex';
+import renderwithRouter from '../helper/renderWithRouter';
+import pokemons from '../data';
+
+describe('Testes da Pokedex page', () => {
+  it('Verifica se a página tem um heading com o texto "Encountered pokémons"', () => {
+    renderwithRouter(<Pokedex />);
+    const titleElement = screen.getByRole('heading', {
+      name: /encountered pokémons/i,
+      level: 2,
+    });
+
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  // it('Verifica se aparece um próximo pokemon ao clicar no botão', () => {
+  //   renderwithRouter(<Pokedex />);
+  //   const btnNext = screen.getByTestId(/next-pokemon/i);
+
+  //   pokemons.forEach(({ name }) => {
+  //     expect(screen.getByText(name)).toBeInTheDocument();
+  //     userEvent.click(btnNext);
+  //   });
+  // });
+});
