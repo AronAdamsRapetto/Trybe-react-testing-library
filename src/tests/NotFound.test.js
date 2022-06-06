dc24c098ef06fb08b1aaa9a36b6a49dc8b97bcac
Aron Adams <aronrapetto@hotmail.com>
2022-06-06 19:10:09 -0300

    requisito 4 concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderwithRouter from '../helper/renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testa NotFound page', () => {
+  it('Verifica se existe o texto "Page requested not found 😭"', () => {
+    renderwithRouter(<NotFound />);
+    const titleElement = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it('Verifica se existe uma imagem específica na tela', () => {
+    renderwithRouter(<NotFound />);
+    const imageElement = screen.getByAltText(/pikachu crying/i);
+
+    expect(imageElement).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
