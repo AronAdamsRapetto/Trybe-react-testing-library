c7b0b7fb678f4c10a3ae2af23ff7ca12f0b24901
Aron Adams <aronrapetto@hotmail.com>
2022-06-06 23:25:54 -0300

    requisito 5 concluido

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Pokedex from '../pages/Pokedex';
+import App from '../App';
 import renderwithRouter from '../helper/renderWithRouter';
 import pokemons from '../data';
 
 describe('Testes da Pokedex page', () => {
   it('Verifica se a página tem um heading com o texto "Encountered pokémons"', () => {
-    renderwithRouter(<Pokedex />);
+    renderwithRouter(<App />);
     const titleElement = screen.getByRole('heading', {
       name: /encountered pokémons/i,
       level: 2,
@@ -16,13 +16,41 @@ describe('Testes da Pokedex page', () => {
     expect(titleElement).toBeInTheDocument();
   });
 
-  // it('Verifica se aparece um próximo pokemon ao clicar no botão', () => {
-  //   renderwithRouter(<Pokedex />);
-  //   const btnNext = screen.getByTestId(/next-pokemon/i);
+  it('Verifica se aparece um próximo pokemon ao clicar no botão', () => {
+    renderwithRouter(<App />);
+    const btnNext = screen.getByTestId(/next-pokemon/i);
 
-  //   pokemons.forEach(({ name }) => {
-  //     expect(screen.getByText(name)).toBeInTheDocument();
-  //     userEvent.click(btnNext);
-  //   });
-  // });
+    pokemons.forEach(({ name }) => {
+      expect(screen.getByText(name)).toBeInTheDocument();
+      userEvent.click(btnNext);
+      expect(screen.queryByText(name)).not.toBeInTheDocument();
+    });
+  });
+
+  it('Verifica se Pokédex tem todos os botões de filtro na tela', () => {
+    renderwithRouter(<App />);
+    const allBtnTypes = screen.getAllByTestId('pokemon-type-button');
+    const allTypes = pokemons.reduce((types, { type }) => [...types, type], [])
+      .filter((type, index, self) => self.indexOf(type) === index);
+
+    allTypes.forEach((type, index) => {
+      expect(screen.getByRole('button', { name: /all/i })).toBeInTheDocument();
+      expect(allBtnTypes[index]).toBeInTheDocument();
+      expect(allBtnTypes[index]).toHaveTextContent(type);
+    });
+  });
+
+  it('Verifica se existe um botão para resetar os filtros', () => {
+    renderwithRouter(<App />);
+    const resetFiltersBtn = screen.getByRole('button', { name: /all/i });
+
+    expect(resetFiltersBtn).toBeInTheDocument();
+
+    userEvent.click(resetFiltersBtn);
+
+    pokemons.forEach(({ name }) => {
+      expect(screen.getByText(name)).toBeInTheDocument();
+      userEvent.click(screen.getByRole('button', { name: /próximo pokémon/i }));
+    });
+  });
 });
